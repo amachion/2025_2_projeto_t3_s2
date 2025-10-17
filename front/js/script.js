f44cb1234ac457b7ce290bd44dd2a59782887e2f
Andreia Machion <amachion@gmail.com>
2025-10-17 09:12:18 -0300

    apostila 09 - página 24

--- front/js/script.js
+++ front/js/script.js
@@ -5,5 +5,15 @@ const filmesEndpoint = '/filmes'
 async function obterFilmes () {
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     const filmes = (await axios.get(URLcompleta)).data
-    console.log(filmes)
+    //console.log(filmes)
+    let tabela = document.querySelector('.filmes') //busca pelo elemento que tem a classe filmes
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0] //se posiciona no filho tbody
+    //para cada filme no vetor de filmes, criar linha, criar colunas e adicionar o conteúdo
+    for (let filme of filmes)  {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 }
